90e618291b97d5ade7d5c6809c836814967b0b78
ssharma471 <sidhantsharmavlog@gmail.com>
2024-04-10 00:30:00 -0400

    added

---
+++ src/constants/index.js
@@ -0,0 +1,306 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  python,
+  cplus
+
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Frontend Developer",
+    icon: creator,
+  },
+  {
+    title: "Python Developer",
+    icon: mobile,
+  }, {
+    title: "Database Design & Management",
+    icon: web,
+  }, {
+    title: "User Interface Design",
+    icon: backend,
+  }, {
+    title: "C/C++ development",
+    icon: mobile,
+  }
+  , {
+    title: "IOS and Android Development",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "C++",
+    icon: cplus,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Manager",
+    company_name: "Brar's Eats and Sweets",
+    icon: starbucks,
+    iconBg: "black",
+    date: "March 2021 - Present",
+    points: [
+      "Effectively managed a dynamic team of 20 individuals, providing leadership, guidance, and support to enhance performance.",
+      "Oversaw all aspects of restaurant operations, ensuring seamless day-to-day activities and maintaining high standards of service.",
+      "Applied strong research and problem-solving skills to address operational challenges, resulting in enhanced efficiency and customer satisfaction",
+      "Successfully led and participated in various projects, showcasing adept project management abilities both within a team and individually",
+      "Demonstrated a detail-oriented approach in creating accurate and punctual reports, contributing to the precision of business documentation.",
+      "Delivered impactful presentations, leveraging effective communication skills to convey key information and insights.",
+    ],
+  },
+  {
+    title: "Metropolitan-Museum-of-Art",
+    company_name: "Technical Project",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Tech Stack: Next.js, ReactJs, NodeExpress",
+    points: [
+      "Independently developed a robust Web API using Express and Node.js, facilitating efficient data management and enhancing the museum's online presence. Utilized Next.js for the front end and integrated a MongoDB database to create a modern, responsive website that improved user engagement and provided a dynamic online experience for museum visitors."
+      ,"GitHub Link:https://github.com/ssharma471/Metropolitan_Art_Museum",
+    ],
+  },
+  {
+    title: "Ticket Kart",
+    company_name: "Technical Project",
+    icon: meta,
+    iconBg: "yellow",
+    date: "Tech Stack: Python, Django, SQLITE",
+    points: [
+      "The vision for TicketKart is to provide a reliable, efficient, and user-friendly platform for concert promoters and customers, enabling them to seamlessly manage events, sell and purchase tickets, and select preferred seats. Comprehensive solution includes the ability to add venue information, making TicketKart the go-to platform for all concert-related needs. My goal is to make TicketKart the most trusted and preferred ticket sales system in the market."
+      ,"GitHub Link: https://github.com/ssharma471/TicketMaster"
+
+    ],
+  },
+  {
+    title: "Portfolio",
+    company_name: "Technical Project",
+    icon: meta,
+    iconBg: "#383E56",
+    date: "Tech Stack: Next.js, ReactJs, NodeExpress",
+    points: [
+      "Leveraged Reacts component-based architecture to develop a dynamic, responsive UI for a portfolio website, ensuring seamless user experience across various devices. Employed React.js best practices for creating modular, reusable components, coupled with individual CSS files for effective style management. Enhanced the application's performance and intuitiveness using React Hooks and crafted a personalized user journey with interactive elements like Download Link and Contact ME. Maintained a clean, efficient codebase, adhering to industry standards for readability and maintainability.",
+      "GitHub Link: https://github.com/ssharma471/Portfolio ",
+      "Vercel Link: https://portfolio-self-zeta-85.vercel.app/",
+    ],
+  },
+  {
+    title: "Capstone Project",
+    company_name: "Astrology Website",
+    icon: meta,
+    iconBg: "green",
+    date: "Tech Stack: Next.js, ReactJs, NodeExpress, MongoDb, JavaScript",
+    points: [
+      "For my capstone project, I am developing an astrology website aimed at providing personalized astrological insights and services. The website will offer various tools and features, including distinct astrology packages, user authentication, bookings page, newsletter subscription, blog section, user profiles, astrologer profiles, and a contact us page. To achieve this, I am utilizing a modern tech stack consisting of React for the frontend, Next.js for server-side rendering, and Bootstrap for responsive design. On the backend, I am employing Node.js with Express for server-side logic and MongoDB for database management. Additionally, Stripe is integrated for payment processing, ensuring a seamless user experience. This project aims to create a user-friendly and interactive platform for astrology enthusiasts to explore and engage with astrological services.",
+    ],
+  },
+];
+
+const testimonials = [
+  // {
+  //   testimonial:
+  //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  //   name: "Sara Lee",
+  //   designation: "CFO",
+  //   company: "Acme Co",
+  //   image: "https://randomuser.me/api/portraits/women/4.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: "Chris Brown",
+  //   designation: "COO",
+  //   company: "DEF Corp",
+  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: "Lisa Wang",
+  //   designation: "CTO",
+  //   company: "456 Enterprises",
+  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+  // },
+];
+
+const projects = [
+  {
+    name: "Metropolitan-Museum-of-Art",
+    description:
+    "Independently developed a robust Web API using Express and Node.js, facilitating efficient data management and enhancing the museum's online presence. Utilized Next.js for the front end and integrated a MongoDB database to create a modern, responsive website that improved user engagement and provided a dynamic online experience for museum visitors.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "NextJs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "NodeExpress",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Portfolio",
+    description:
+    "Leveraged Reacts component-based architecture to develop a dynamic, responsive UI for a portfolio website, ensuring seamless user experience across various devices. Employed React.js best practices for creating modular, reusable components, coupled with individual CSS files for effective style management. Enhanced the application's performance and intuitiveness using React Hooks and crafted a personalized user journey with interactive elements like Download Link and Contact ME. Maintained a clean, efficient codebase, adhering to industry standards for readability and maintainability.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "NectJs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "NodeExpress",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Tailwind Css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Capstone Project - Astrology Webiste",
+    description:
+    "For my capstone project, I am developing an astrology website aimed at providing personalized astrological insights and services. The website will offer various tools and features, including distinct astrology packages, user authentication, bookings page, newsletter subscription, blog section, user profiles, astrologer profiles, and a contact us page. To achieve this, I am utilizing a modern tech stack consisting of React for the frontend, Next.js for server-side rendering, and Bootstrap for responsive design. On the backend, I am employing Node.js with Express for server-side logic and MongoDB for database management. Additionally, Stripe is integrated for payment processing, ensuring a seamless user experience. This project aims to create a user-friendly and interactive platform for astrology enthusiasts to explore and engage with astrological services.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MongoDb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
